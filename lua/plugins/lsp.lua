3239cd4f314d9fa73a309e26112c3809bf398abd
SparkyPotato <noob.sparkypotato@gmail.com>
2024-06-13 14:47:10 -0500

    use local LSP

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,23 @@
 local function config()
+	local lspconfig = require("lspconfig")
 	require("mason").setup({ PATH = "append" })
-	require("mason-lspconfig").setup()
 
 	local servers = {
-		-- clangd = {},
-		-- gopls = {},
-		-- pyright = {},
-		-- tsserver = {},
-		-- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
 		clangd = {},
-		rust_analyzer = {},
+		rust_analyzer = {
+			["rust-analyzer"] = {
+				checkOnSave = {
+					command = "check"
+				}
+			}
+		},
 		lua_ls = {
 			Lua = {
 				workspace = { checkThirdParty = false },
 				telemetry = { enable = false },
-			},
+			}
 		},
 	}
-
-	require('neodev').setup()
 
 	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
@@ -27,15 +25,15 @@ local function config()
 		dynamicRegistration = false,
 		lineFoldingOnly = true,
 	}
-	local mason_lspconfig = require("mason-lspconfig")
 
-	mason_lspconfig.setup {
-		ensure_installed = vim.tbl_keys(servers),
+	lspconfig.rust_analyzer.setup {
+		settings = servers["rust_analyzer"]
 	}
 
+	local mason_lspconfig = require("mason-lspconfig")
 	mason_lspconfig.setup_handlers {
 		function(server_name)
-			require("lspconfig")[server_name].setup {
+			lspconfig[server_name].setup {
 				capabilities = capabilities,
 				on_attach = require("mappings").lsp,
 				settings = servers[server_name],
@@ -52,6 +50,6 @@ return {
 		{ "williamboman/mason.nvim", config = true },
 		"williamboman/mason-lspconfig.nvim",
 		{ "j-hui/fidget.nvim",       opts = {} },
-		"folke/neodev.nvim",
+		"folke/lazydev.nvim",
 	}
 }
